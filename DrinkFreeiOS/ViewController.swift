98ba1f4599490ef331024c41fdbbbc7922a3bd03
izhang1 <ivanzhangemail@gmail.com>
2016-02-27 11:46:25 -0500

    creating new ios app

---
+++ DrinkFreeiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrinkFreeiOS
+//
+//  Created by Ivan Zhang on 2/27/16.
+//  Copyright © 2016 NovusApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
