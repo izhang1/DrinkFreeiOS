e94e084d3ba7674f434dba5018168dc024d275d5
izhang1 <ivanzhangemail@gmail.com>
2016-02-28 20:44:50 -0500

    Installed new Firebase apnew Firebase install

--- DrinkFreeiOS/ViewController.swift
+++ DrinkFreeiOS/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 NovusApp. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
---
+++ Pods/Target Support Files/Pods-DrinkFreeiOS/Pods-DrinkFreeiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DrinkFreeiOS : NSObject
+@end
+@implementation PodsDummy_Pods_DrinkFreeiOS
+@end
